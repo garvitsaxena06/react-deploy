e3fa0d24e1cce8c7f356d1ee53f05280bc40ba60
unknown <garvitsaxena06@gmail.com>
2020-04-05 14:49:42 +0530

    Updates

---
+++ precache-manifest.59742f22a9174aaa603a7cd9c376135d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf9263db9102eea5393488c164b7743",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "1598c850c275b96c5ee2",
+    "url": "/react-deploy/static/css/main.ca60345f.chunk.css"
+  },
+  {
+    "revision": "f9c28ca105402576ab28",
+    "url": "/react-deploy/static/js/2.958100db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.958100db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1598c850c275b96c5ee2",
+    "url": "/react-deploy/static/js/main.b611dc02.chunk.js"
+  },
+  {
+    "revision": "ab506f364532d6f7992c",
+    "url": "/react-deploy/static/js/runtime-main.1f0e46da.js"
+  },
+  {
+    "revision": "49e95ca28f50eb7e16de3aab0ef8529b",
+    "url": "/react-deploy/static/media/my-img.49e95ca2.jpeg"
+  }
+]);
